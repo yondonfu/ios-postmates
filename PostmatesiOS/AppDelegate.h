17491abfd4d71aa18b593b46fca93b132a908868
Snaheth <thumathysnaheth@gmail.com>
2015-10-10 00:31:52 -0700

    Initial Commit

---
+++ PostmatesiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PostmatesiOS
+//
+//  Created by Snaheth Thumathy on 10/10/15.
+//  Copyright © 2015 Cal Hacks Squad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PostmatesiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PostmatesiOS
+//
+//  Created by Snaheth Thumathy on 10/10/15.
+//  Copyright © 2015 Cal Hacks Squad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
